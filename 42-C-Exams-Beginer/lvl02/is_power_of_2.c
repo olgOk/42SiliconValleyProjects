dd388aaa7457847a8af0cfd0be423277febd2843
Olga Okrut <46659064+olgOk@users.noreply.github.com>
2023-03-28 10:22:15 -0700

    Update is_power_of_2.c
    missing ")" fix

--- 42-C-Exams-Beginer/lvl02/is_power_of_2.c
+++ 42-C-Exams-Beginer/lvl02/is_power_of_2.c
@@ -20,5 +20,5 @@ Your function must be declared as follows: int  is_power_of_2(unsigned int n);  
 
 int        is_power_of_2(unsigned int n)
 {
-    return ((n != 0) && ((n & (n - 1)) == 0);
+    return ((n != 0) && ((n & (n - 1)) == 0));
 }
